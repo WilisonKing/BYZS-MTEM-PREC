9c868e6ccde86ca0dcb32368ff79d675681c74b9
WilisonKing <2020518360@qq.com>
2020-04-21 15:19:20 +0800

    ALL

--- PRE&TEM/prec/prec6.0.py
+++ PRE&TEM/prec/prec6.0.py
@@ -12,18 +12,20 @@ import numpy as np
 import netCDF4
 from netCDF4 import Dataset as D
 import os
+import math
 os.environ["CUDA_VISIBLE_DEVICES"]='2'
 
 
 # In[ ]:
 
 
-BATCH_SIZE_TRAIN=64
+BATCH_SIZE_TRAIN=128
 BATCH_SIZE_TEST=128
-LR=0.0006
-EPOCH=40
+LR=0.0001
+EPOCH=30
 Name='Conv2d_prec6.0'
 basic_day=3
+step_print=20
 seed=2
 torch.cuda.manual_seed(seed)#为当前GPU设置随机种子 
 
@@ -91,16 +93,16 @@ for i in range(10):
     maxt = t[:, i, :, :].max()
     t[:, i, :, :] = (t[:, i, :, :] - mint) / (maxt - mint)
 
-t=t*get_prec_mask()
+#t=get_prec_mask()*t
 
 
 l=fo.variables['PREC'][:]
 
 minl = l[:, :, :].min()
 maxl = l[:, :, :].max()
-l[:, :, :] = (l[:, :, :] - mint) / (maxt - mint)
+l[:, :, :] = (l[:, :, :] - minl) / (maxl - minl)
 
-l=l*get_prec_mask()
+#l=get_prec_mask()*l
 # In[ ]:
 
 
@@ -111,14 +113,14 @@ print(l.shape)
 
 
 traini=np.array(t[:365*30,:,:,:])
-testi=np.array(t[365*30:,:,:,:])
+testi=np.array(t[365*30:365*37,:,:,:])
 trainl=np.array(l[:245*30,:,:])
 testl=np.array(l[245*30:,:,:])
 
-
-# In[ ]:
-
-
+#traini=np.array(t[:365*10,:,:,:])
+#testi=np.array(t[365*10:365*13,:,:,:])
+#trainl=np.array(l[:245*10,:,:])
+#testl=np.array(l[245*10:245*13,:,:])
 print(traini.shape,testi.shape,trainl.shape,testl.shape)
 
 
@@ -130,16 +132,18 @@ class MyDataset(Dataset):
         self.data = data
         self.label = label
         self.redata = data.reshape(-1,365,10,81,91)
-        self.relabel = label.reshape(-1,245,81,91)
+        #self.relabel = label.reshape(-1,245,81,91)
     def __len__(self):
         return len(self.label)
     def __getitem__(self, idx):
         for i in range(len(self.label)//245):
             if i-1<idx/245.0 and i+1>idx/245:
                 year=i
+                #print('idx:',idx,'\n','year:',year)
                 break
         datamerge = self.redata[year,57+idx-i*245:57+idx+basic_day-i*245,:,:,:].reshape(-1,basic_day*10 , 81, 91)
-        return np.squeeze(datamerge), self.relabel[year,idx-i*245,:,:]
+        #print('单个数据加载',datamerge.shape,np.squeeze(self.label[idx,:,:].shape))
+        return np.squeeze(datamerge), np.squeeze(self.label[idx,:,:])
 
 
 # In[ ]:
@@ -182,20 +186,66 @@ loss_function = nn.MSELoss()
 # In[ ]:
 
 
+#def cos_sim(v1, v2):
+#    vector1=v1.reshape(-1,)
+#    vector2=v2.reshape(-1,)
+#    dot_product = 0.0
+#    normA = 0.0
+#    normB = 0.0
+#    for a, b in zip(vector1, vector2):
+#        dot_product += a * b
+#        normA += a ** 2
+#        normB += b ** 2
+#    if normA == 0.0 or normB == 0.0:
+#        return None
+#    else:
+#        return dot_product / ((normA * normB) ** 0.5)
+
+#def cos_sim(v1, v2):
+#    cossim=0
+#    print('余弦相似开始')
+#    for i in range(len(v1)):
+#        vector1=torch.squeeze(v1[i,:,:]).view(-1,)
+#        vector2=torch.squeeze(v2[i,:,:]).view(-1,)
+#        dot_product = 0.0
+#        normA = 0.0
+#        normB = 0.0
+#        for a, b in zip(vector1, vector2):
+#            #if a<0 or b<0:
+#            #    print('a:',a,'  b:',b)
+#            #    exit()
+#            dot_product += a * b
+#            normA += a ** 2
+#            normB += b ** 2
+#        
+#        cossim+=dot_product / ((normA * normB) ** 0.5)
+#    print('余弦相似结束')
+#    return cossim/len(v1)
+
+class CosineSimilarity(nn.Module):
+    def forward(self, tensor_1, tensor_2):
+        normalized_tensor_1 = tensor_1 / tensor_1.norm(dim=-1, keepdim=True)
+        normalized_tensor_2 = tensor_2 / tensor_2.norm(dim=-1, keepdim=True)
+        return (normalized_tensor_1 * normalized_tensor_2).sum(dim=-1)
+c_s=CosineSimilarity()
+
 def cos_sim(v1, v2):
-    vector1=v1.reshape(-1,)
-    vector2=v2.reshape(-1,)
-    dot_product = 0.0
-    normA = 0.0
-    normB = 0.0
-    for a, b in zip(vector1, vector2):
-        dot_product += a * b
-        normA += a ** 2
-        normB += b ** 2
-    if normA == 0.0 or normB == 0.0:
-        return None
-    else:
-        return dot_product / ((normA * normB) ** 0.5)
+    cossim=0
+    #print('余弦相似开始')
+    for i in range(len(v1)):
+        vector1=torch.squeeze(v1[i,:,:]).view(-1,)
+        vector2=torch.squeeze(v2[i,:,:]).view(-1,)
+#        dot_product = 0.0
+#        normA = 0.0
+#        normB = 0.0
+#        for a, b in zip(vector1, vector2):
+#            dot_product += a * b
+#            normA += a ** 2
+#            normB += b ** 2
+#        cossim+=dot_product / ((normA * normB) ** 0.5)
+        cossim+=c_s(vector1, vector2)
+    #print('余弦相似结束')
+    return cossim/len(v1)
 
 
 # In[ ]:
@@ -203,10 +253,15 @@ def cos_sim(v1, v2):
 
 from torch.autograd import Variable # 获取变量
 flag=0
+cosmember=0
 for epoch in range(EPOCH): 
+    #print('epoch=%d开始:'%epoch)
     running_loss=0.0
     running_cos=0.0
+    if cosmember>0.90:
+        break
     for step,(x,y) in enumerate(train_loader): 
+        #print('step=%d已读取数据'%step)
         if flag==1:
             break
         b_x = Variable(x).cuda() 
@@ -214,28 +269,34 @@ for epoch in range(EPOCH):
         output = net(b_x.float()) 
         optimizer.zero_grad() 
         loss = loss_function(output, b_y)
+        #print('loss end:',loss)
         loss.backward() 
         optimizer.step() 
         running_loss += loss.item()
         #print(output.shape,b_y.shape)
         running_cos +=cos_sim(output,b_y)
-        if step % 30 == 29: 
-            print('Train||','Epoch:', epoch, '|Step:', step, '|train loss:%.6f'%(running_loss/30.0),                  '|余弦相似度:%.6f'%(running_cos/30.0))
-            cosmember=running_cos/30.0
+        #print('running_cos end:',running_cos)
+        if step % step_print == step_print-1: 
+            print('Train||','Epoch:', epoch, '|Step:', step, '|train loss:%.6f'%(running_loss/step_print),'|余弦相似度:%.6f'%(running_cos/step_print))
+            cosmember=running_cos/step_print
             running_loss =0.0
             running_cos=0.0
-    if epoch>EPOCH-5 or running_cos>0.80 or flag==1:
+    if epoch>EPOCH-5 or cosmember>0.90 or flag==1:
+        #print('进入测试(编号:%d):'%epoch)
         flag=1
         t_loss=0.0
         t_cos=0.0
         for k,(m,n) in enumerate(test_loader): 
+            #print('正在迭代测试  ','k=',k)
             t_x = Variable(m).cuda() 
             t_y = Variable(n).cuda()
             output2 = net(t_x.float()) 
             loss = loss_function(output2 , t_y)
             t_loss += loss.item()
             t_cos +=cos_sim(output2,t_y)
-        print( '第%d轮结束后:'%epoch,'|test_loss:%.6f'%(t_loss/(k+1)),              '|余弦相似度:%.6f'%(t_cos/(k+1)))
+            #print('迭代测试结束  ','k=',k)
+        print( '第%d轮结束后:'%epoch,'|test_loss:%.6f'%(t_loss/(k+1)),'|余弦相似度:%.6f'%(t_cos/(k+1)))
+        #print('测试结束(编号:%d):'%epoch)
 print(str(Name)+'_BSTRAIN',str(BATCH_SIZE_TRAIN),'_BSTEST',str(BATCH_SIZE_TEST),'_LR',str(LR),'_EPOCH',str(EPOCH))
 
 
